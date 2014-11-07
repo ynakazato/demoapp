16ecc61a2a89e9df17277bbd4e3ff6ca1e38140a
ynakazato <yuki_nakazato@mba.berkeley.edu>
2014-11-07 01:24:39 +0000

    Created users like a boss

---
+++ class2.rb
@@ -0,0 +1,13 @@
+class Greeter
+  def initialize(name="World")
+    @name = name
+  end
+  def say_hi
+    puts "hi #{@name}"
+  end
+  def say_bye
+    puts "see ya later #{@name}"
+  end
+end
+
+    
